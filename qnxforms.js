38530f389a7bdce661c1de9ae3f2e4af1ab0ea46
Marco van Hylckama Vlieg <marco@i-marco.nl>
2011-06-04 18:26:06 -0700

    Selects now sort of work. Still need to handle clicks outside the selects.

--- qnxforms.js
+++ qnxforms.js
@@ -1,7 +1,7 @@
 function qnxwidget(type, element, args) {
 	var element = element;
 	var container = element.parent();
-	var detachedElement;
+	var detachedElement, options, dropdownhtml;
 	function buttonTouchStart(element) {
 		$(element.parentNode).addClass('active');
 	}
@@ -85,16 +85,64 @@ function qnxwidget(type, element, args) {
 				}
 			}
 			});
-		break;		
+		break;	
+		case 'select':
+		container.append(detachedElement.addClass('hidden'));
+		container.html(container.html() + '<div class="qnxselectholder"><div class="qnxselectinner"><div class="qnxselectright"><div class="arrowholder"><div class="arrow"></div></div></div></div></div><div class="qnxselectdropdown hidden"><ul></ul></div>');
+		options = element.find('option');
+		activeItem = $(options[0]).html();
+		dropdownhtml = '';
+		for(i=0;i<options.length;i++) {
+			dropdownhtml = dropdownhtml + '<li>' + $(options[i]).html() + '</li>';
+			if($(options[i]).attr('selected')) {
+				activeItem = $(options[i]).html();
+			}
+		}
+		activeItem = '<span>' + activeItem + '</span>';
+		container.addClass('relative');
+		container.find('ul').html(dropdownhtml);
+		container.find('.qnxselectinner').html(container.find('.qnxselectinner').html() + activeItem);
+		container.find('.qnxselectdropdown').removeClass('hidden');
+		selectWidth = container.find('.qnxselectdropdown ul').get(0).offsetWidth;
+		container.find('.qnxselectdropdown').addClass('hidden').find('ul').attr('style', 'width:' + (selectWidth + 35) + 'px');
+		container.find('.qnxselectdropdown').attr('style', 'left:' + container.find('.qnxselectholder').get(0).offsetLeft + 'px');
+		container.find('.qnxselectholder').attr('style', 'width:' + (selectWidth) + 'px');
+		container.find('.qnxselectholder').click(function(e) {
+			$(document).find('.qnxselectholder').addClass('lowered');
+			container.find('.qnxselectholder').removeClass('lowered');
+			var options = container.find('.qnxselectdropdown ul li');
+			var realoptions = element.find('option');
+			realoptions.removeAttr('selected');
+			options.removeClass('active');
+			var currentSelectedItem = container.find('.qnxselectinner span').html();
+			for(i=0;i<options.length;i++) {
+				if($(options[i]).html() == currentSelectedItem) {
+					$(options[i]).addClass('active');
+					$(realoptions[i]).get(0).setAttribute('selected', '');
+				}
+			}
+			
+			container.find('.qnxselectdropdown').toggleClass('hidden');
+			container.find('.qnxselectholder').toggleClass('active');
+		});
+		container.find('.qnxselectdropdown').click(function(e) {
+		container.find('.qnxselectinner span').html($(e.target).html());
+		console.log($(e.target).html());
+		container.find('.qnxselectholder').click();
+		$(document).find('.qnxselectholder').removeClass('lowered');
+		});
+		break;	
+		
 	}
 };
 
 
 $(document).ready(function() {	
 	var qnxelements = ($('.qnx'));
+	
 	qnxelements.each(function() {
+		console.log($(this).get(0).nodeName);		
 		if(($(this).attr('type') == 'submit') || ($(this).attr('type') == 'button') || ($(this).get(0).nodeName == 'BUTTON')) {
-			console.log('ping');
 			qnxwidget('button', $(this), {});
 		}
 		if($(this).attr('type') == 'text') {
@@ -105,6 +153,9 @@ $(document).ready(function() {
 		}
 		if(($(this).attr('type') == 'checkbox') && $(this).hasClass('check')) {
 			qnxwidget('checkbox', $(this), {});
+		}
+		if(($(this).get(0).nodeName == 'SELECT')) {
+			qnxwidget('select', $(this), {});
 		}
 	});
 });
